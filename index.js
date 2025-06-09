18c1a41650e27036c296e170608c6a3954615fa1
Mahran <benkhamsamahran@isamm.u-manouba.tn>
2025-06-09 16:37:30 +0100

    added a post handler to handle file upload

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var bodyParser = require('body-parser');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file){
+    return res.status(400).json({ error: 'No file has been uploaded !!!' });
+  } 
+  
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
